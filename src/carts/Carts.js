cc906a81e138642550d7a9c46fb8bb05a85f6aa2
nabanitadutta874 <65589555+nabanitadutta874@users.noreply.github.com>
2022-10-11 13:16:59 +0530

    1 file modified

--- src/carts/Carts.js
+++ src/carts/Carts.js
@@ -6,22 +6,26 @@ import CartItem from './cartItem/CartItem';
 
 function Carts() {
   const state = useSelector((state) => state);
+  console.log(state.itemList);
   const products = state.itemList.includes(' ')
     ? state.itemList.split(' ')
-    : [state.itemList];
+    : state.itemList;
   return (
     <Container>
       <Card style={{ marginTop: '5px' }}>
         <Card.Body>
           <Card.Title>Cart Details</Card.Title>
           <Row>
-            {products.map((elem, index) => {
-              return (
-                <Col sm={4} key={index} style={{ margin: 0 }}>
-                  <CartItem prodKey={elem} />
-                </Col>
-              );
-            })}
+            {products.length > 1
+              ? products.map((elem, index) => {
+                  console.log(elem);
+                  return (
+                    <Col sm={4} key={index} style={{ margin: 0 }}>
+                      <CartItem prodKey={elem} />
+                    </Col>
+                  );
+                })
+              : 'no items to show'}
           </Row>
         </Card.Body>
       </Card>
